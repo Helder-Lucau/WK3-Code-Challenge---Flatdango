a323cc77cbf2d0646a563b62f1b99fc2d691c031
Helder <helderlucau@gmail.com>
2023-07-02 21:54:32 +0300

    function to fetch all movies

--- js/main.js
+++ js/main.js
@@ -1,31 +1,58 @@
-//poster, title, runtime, showtime, and available tickets
-
+//db.json api URL
 const apiURL = 'http://localhost:3000/films';
 
 //function that fetch the first movie data from the JSON db
-function getFilmsData(){
-    fetch('http://localhost:3000/films/1')
+function getFirstMovie(){
+
+    fetch(`${apiURL}/1`)
     .then((response) => response.json())
-    .then((movieData) => displayFirstMovie(movieData))
-}
-getFilmsData()
+    .then(movieData => {
 
-function displayFirstMovie(movieData){
-
-    let movieCard = document.createElement('li');
-    movieCard.className = 'movie-card'
-    movieCard.innerHTML = `
-        <img src="${movieData.poster}" alt="poster">
-        <div class = "content">
-            <h4>${movieData.title}</h4>
-            <p><span>${movieData.runtime}</span>
-            <p><span>${movieData.showtime}</span>
-            <button>${movieData.tickets_sold}</button>
-        </div>        
+        let movieCard = document.createElement('li');
+        movieCard.className = 'movie-card'
+        movieCard.innerHTML = `
+            <img src="${movieData.poster}" alt="poster">
+            <div class = "content">
+                <h3>${movieData.title}</h3>
+                <p><span>Duration: ${movieData.runtime} min</span>
+                <p><span>Showtimes: ${movieData.showtime}</span>
+            </div>
+            <div>
+                <button>Available Tickets: ${movieData.tickets_sold}</button>
+            </div>
     `
-    document.querySelector('#movie-list').appendChild(movieCard)
+    document.querySelector('#first-movie').appendChild(movieCard)
+    })
 }
 
-// function movieDetails(){
+//function that fetch all the movies from db.json
+function movieMenu(){
     
-// }
+    fetch(apiURL)
+    .then((res) => res.json())
+    .then(data => {
+        
+        data.forEach(movieMenu => {
+            let menu = document.createElement('li');
+            menu.className = 'movie-menu'
+            menu.innerHTML = `
+            <img src="${movieMenu.poster}" alt="poster">
+            <div class = "movie-content">
+                <h3>${movieMenu.title}</h3>
+                <p><span>Duration: ${movieMenu.runtime} min</span>
+                <p><span>Showtimes: ${movieMenu.showtime}</span>
+            </div>
+            <div>
+                <button>Available Tickets: ${movieMenu.tickets_sold}</button>
+            </div>
+    `
+            document.querySelector('#all-movie-list').appendChild(menu)
+        })
+    })
+}
+
+function initialize(){
+    getFirstMovie()
+    movieMenu()
+}
+initialize()
