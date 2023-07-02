b69f34ad31767722c7ee8cc5ba4f94b8b127b418
Helder <helderlucau@gmail.com>
2023-07-02 18:58:04 +0300

    get first movie function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
+//poster, title, runtime, showtime, and available tickets
+
 const apiURL = 'http://localhost:3000/films';
 
-//function that fetch the data from the JSON db
+//function that fetch the first movie data from the JSON db
 function getFilmsData(){
-    fetch(apiURL)
+    fetch('http://localhost:3000/films/1')
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((movieData) => displayFirstMovie(movieData))
 }
 getFilmsData()
+
+function displayFirstMovie(movieData){
+
+    let movieCard = document.createElement('li');
+    movieCard.className = 'movie-card'
+    movieCard.innerHTML = `
+        <img src="${movieData.poster}" alt="poster">
+        <div class = "content">
+            <h4>${movieData.title}</h4>
+            <p><span>${movieData.runtime}</span>
+            <p><span>${movieData.showtime}</span>
+            <button>${movieData.tickets_sold}</button>
+        </div>        
+    `
+    document.querySelector('#movie-list').appendChild(movieCard)
+}
+
+// function movieDetails(){
+    
+// }
